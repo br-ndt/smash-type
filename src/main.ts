83d24639d99c6eeea471d125f4e1409cc68e23a7
br-ndt <br.ndt.tyler@gmail.com>
2024-12-11 00:07:35 -0500

    knockback collisions, health affects knockback

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,39 @@
 import * as w4 from "./wasm4";
 
-// Define the smiley sprite data
-const smiley = memory.data<u8>([
-    0b11000011,
-    0b10000001,
-    0b00100100,
-    0b00100100,
-    0b00000000,
-    0b00100100,
-    0b10011001,
-    0b11000011,
+// Define the p1 sprite data
+const p1 = memory.data<u8>([
+  0b11000011, 0b10000001, 0b00100100, 0b00100100, 0b00000000, 0b00100100,
+  0b10011001, 0b11000011,
 ]);
 
-// Define the inverted smiley sprite data by flipping each byte's bits
-const invertedSmiley = memory.data<u8>([
-    ~0b11000011 & 0xff, // 00111100
-    ~0b10000001 & 0xff, // 01111110
-    ~0b00100100 & 0xff, // 11011011
-    ~0b00100100 & 0xff, // 11011011
-    ~0b00000000 & 0xff, // 11111111
-    ~0b00100100 & 0xff, // 11011011
-    ~0b10011001 & 0xff, // 01100110
-    ~0b11000011 & 0xff, // 00111100
+// Define the inverted p1 sprite data by flipping each byte's bits
+const p2 = memory.data<u8>([
+  ~0b11000011 & 0xff, // 00111100
+  ~0b10000001 & 0xff, // 01111110
+  ~0b00100100 & 0xff, // 11011011
+  ~0b00100100 & 0xff, // 11011011
+  ~0b00000000 & 0xff, // 11111111
+  ~0b00100100 & 0xff, // 11011011
+  ~0b10011001 & 0xff, // 01100110
+  ~0b11000011 & 0xff, // 00111100
 ]);
 
-// Initialize global position variables for the smileys
-let smileyX: i32 = 145;          // Initial X position for smiley
-let smileyY: i32 = 50;           // Initial Y position for smiley
+// Initialize global position variables for the p1s
+let p1X: i32 = 145; // Initial X position for p1
+let p1Y: i32 = 50; // Initial Y position for p1
 
-let invertedSmileyX: i32 = 5;    // Initial X position for invertedSmiley
-let invertedSmileyY: i32 = 50;   // Initial Y position for invertedSmiley
+let p2X: i32 = 5; // Initial X position for p2
+let p2Y: i32 = 50; // Initial Y position for p2
 
-// Initialize velocity variables for the smileys
-let smileyVX: f32 = 0.0;
-let smileyVY: f32 = 0.0;
+// Initialize velocity variables for the p1s
+let p1VX: f32 = 0.0;
+let p1VY: f32 = 0.0;
 
-let invertedSmileyVX: f32 = 0.0;
-let invertedSmileyVY: f32 = 0.0;
+let p2VX: f32 = 0.0;
+let p2VY: f32 = 0.0;
 
-// Define movement speed
-const MOVE_SPEED: i32 = 2; // Pixels per frame
+let freezeMovement = false;
+let unfreezeFrame = 0;
 
 // Define movement acceleration and friction
 const ACCELERATION: f32 = 0.5;
@@ -47,163 +41,233 @@ const FRICTION: f32 = 0.85; // Increased friction
 
 // Function to calculate the distance between two points
 function calculateDistance(x1: i32, y1: i32, x2: i32, y2: i32): f64 {
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-    return Math.sqrt(dx * dx + dy * dy);
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
-// Function to normalize the velocity
-function normalizeVelocity(vx: f32, vy: f32): f32[] {
-    const length = Math.sqrt(vx * vx + vy * vy) as f32;
-    if (length > 1) {
-        return [vx / length, vy / length];
-    }
-    return [vx, vy] as f32[];
+// Function to normalize a vector
+function normalizeVector(vx: f32, vy: f32): Vector {
+  const length = Math.sqrt(vx * vx + vy * vy) as f32;
+  if (length > 0) {
+    return new Vector(vx / length, vy / length);
+  }
+  return new Vector(vx, vy);
 }
+
+class Vector {
+  constructor(public x: f32, public y: f32) {
+    this.x = x;
+    this.y = y;
+  }
+}
+const freezeDuration = 10;
+let currentFrame = 0;
+
+// make variables for p1 and p2 health points
+const MAX_PLAYER_HEALTH: i32 = 10;
+let p1Health: i32 = MAX_PLAYER_HEALTH;
+let p2Health: i32 = MAX_PLAYER_HEALTH;
 
 export function update(): void {
-    // Set the initial draw color palette (foreground color)
-    store<u16>(w4.DRAW_COLORS, 2);
+  if (currentFrame === unfreezeFrame) {
+    freezeMovement = false;
+  }
+  // Set the initial draw color palette (foreground color)
+  store<u16>(w4.DRAW_COLORS, 2);
 
-    // Display game title
-    w4.text("Pizza FIGHTERS!", 25, 10); 
+  // Display game title
+  w4.text("Pizza FIGHTERS!", 25, 10);
 
-    // Retrieve the current state of the gamepads
-    const gamepad1 = load<u8>(w4.GAMEPAD1);
-    const gamepad2 = load<u8>(w4.GAMEPAD2);
+  // Retrieve the current state of the gamepads
+  const gamepad1 = load<u8>(w4.GAMEPAD1);
+  const gamepad2 = load<u8>(w4.GAMEPAD2);
 
-    // Handle movement for smiley using GAMEPAD1 (W, A, S, D)
-    // X-axis movement
-    if (gamepad1 & w4.BUTTON_LEFT) {  
-        smileyVX -= ACCELERATION; // Accelerate left (A)
-    }
-    if (gamepad1 & w4.BUTTON_RIGHT) {   
-        smileyVX += ACCELERATION; // Accelerate right (D)
-    }
+  let p1AX = 0 as f32;
+  let p1AY = 0 as f32;
 
-    // Y-axis movement
-    if (gamepad1 & w4.BUTTON_UP) {    
-        smileyVY -= ACCELERATION; // Accelerate up (W)
-    }
-    if (gamepad1 & w4.BUTTON_DOWN) {  
-        smileyVY += ACCELERATION; // Accelerate down (S)
-    }
+  let p2AX = 0 as f32;
+  let p2AY = 0 as f32;
 
-    // Handle movement for invertedSmiley using GAMEPAD2 (O, K, L, ;)
-    // X-axis movement
-    if (gamepad2 & w4.BUTTON_LEFT) {  
-        invertedSmileyVX -= ACCELERATION; // Accelerate left (O)
-    }
-    if (gamepad2 & w4.BUTTON_RIGHT) {   
-        invertedSmileyVX += ACCELERATION; // Accelerate right (;)
-    }
+  // Handle movement for p1 using GAMEPAD1 (W, A, S, D)
+  // X-axis movement
+  if (gamepad1 & w4.BUTTON_LEFT) {
+    p1AX -= ACCELERATION; // Accelerate left (A)
+  }
+  if (gamepad1 & w4.BUTTON_RIGHT) {
+    p1AX += ACCELERATION; // Accelerate right (D)
+  }
 
-    // Y-axis movement
-    if (gamepad2 & w4.BUTTON_UP) {    
-        invertedSmileyVY -= ACCELERATION; // Accelerate up (K)
-    }
-    if (gamepad2 & w4.BUTTON_DOWN) {  
-        invertedSmileyVY += ACCELERATION; // Accelerate down (L)
-    }
+  // Y-axis movement
+  if (gamepad1 & w4.BUTTON_UP) {
+    p1AY -= ACCELERATION; // Accelerate up (W)
+  }
+  if (gamepad1 & w4.BUTTON_DOWN) {
+    p1AY += ACCELERATION; // Accelerate down (S)
+  }
 
-    // Apply friction to velocities
-    smileyVX *= FRICTION;
-    smileyVY *= FRICTION;
-    invertedSmileyVX *= FRICTION;
-    invertedSmileyVY *= FRICTION;
+  // Handle movement for p2 using GAMEPAD2 (O, K, L, ;)
+  // X-axis movement
+  if (gamepad2 & w4.BUTTON_LEFT) {
+    p2AX -= ACCELERATION; // Accelerate left (O)
+  }
+  if (gamepad2 & w4.BUTTON_RIGHT) {
+    p2AX += ACCELERATION; // Accelerate right (;)
+  }
 
-    // Normalize velocities to prevent faster diagonal movement
-    const smileyVelocity = normalizeVelocity(smileyVX, smileyVY);
-    const invertedSmileyVelocity = normalizeVelocity(invertedSmileyVX, invertedSmileyVY);
+  // Y-axis movement
+  if (gamepad2 & w4.BUTTON_UP) {
+    p2AY -= ACCELERATION; // Accelerate up (K)
+  }
+  if (gamepad2 & w4.BUTTON_DOWN) {
+    p2AY += ACCELERATION; // Accelerate down (L)
+  }
 
-    let normalizedSmileyVX = smileyVX;
-    let normalizedSmileyVY = smileyVY;
-    let invertedNormalizedSmileyVX = invertedSmileyVX;
-    let invertedNormalizedSmileyVY = invertedSmileyVY;
-    
-    // Update positions based on velocities
-    smileyX += normalizedSmileyVX as i32;
-    smileyY += normalizedSmileyVY as i32;
-    invertedSmileyX += invertedNormalizedSmileyVX as i32;
-    invertedSmileyY += invertedNormalizedSmileyVY as i32;
+  const p1NA = normalizeVector(p1AX, p1AY);
+  const p2NA = normalizeVector(p2AX, p2AY);
 
-    // Define screen constraints
-    const SCREEN_WIDTH: i32 = 160;
-    const SCREEN_HEIGHT: i32 = 120; // Common screen height in wasm4
-    const SPRITE_WIDTH: i32 = 8;
-    const SPRITE_HEIGHT: i32 = 8;
+  if (!freezeMovement) {
+    // Update velocities based on acceleration
+    p1VX += p1NA.x;
+    p1VY += p1NA.y;
+    p2VX += p2NA.x;
+    p2VY += p2NA.y;
+  }
 
-    // Constrain smileyX within screen bounds
-    if (smileyX < 0) {
-        smileyX = 0;
-        normalizedSmileyVX = 0;
-    } else if (smileyX > SCREEN_WIDTH - SPRITE_WIDTH) {
-        smileyX = SCREEN_WIDTH - SPRITE_WIDTH;
-        normalizedSmileyVX = 0;
-    }
+  // Apply friction to velocities
+  p1VX *= FRICTION;
+  p1VY *= FRICTION;
+  p2VX *= FRICTION;
+  p2VY *= FRICTION;
 
-    // Constrain smileyY within screen bounds
-    if (smileyY < 0) {
-        smileyY = 0;
-        normalizedSmileyVY = 0;
-    } else if (smileyY > SCREEN_HEIGHT - SPRITE_HEIGHT) {
-        smileyY = SCREEN_HEIGHT - SPRITE_HEIGHT;
-        normalizedSmileyVY = 0;
-    }
+  // Update positions based on velocities
+  p1X += p1VX as i32;
+  p1Y += p1VY as i32;
+  p2X += p2VX as i32;
+  p2Y += p2VY as i32;
 
-    // Constrain invertedSmileyX within screen bounds
-    if (invertedSmileyX < 0) {
-        invertedSmileyX = 0;
-        invertedNormalizedSmileyVX = 0;
-    } else if (invertedSmileyX > SCREEN_WIDTH - SPRITE_WIDTH) {
-        invertedSmileyX = SCREEN_WIDTH - SPRITE_WIDTH;
-        invertedNormalizedSmileyVX = 0;
-    }
+  // Define screen constraints
+  const SCREEN_WIDTH: i32 = 160;
+  const SCREEN_HEIGHT: i32 = 120; // Common screen height in wasm4
+  const SPRITE_WIDTH: i32 = 8;
+  const SPRITE_HEIGHT: i32 = 8;
 
-    // Constrain invertedSmileyY within screen bounds
-    if (invertedSmileyY < 0) {
-        invertedSmileyY = 0;
-        invertedNormalizedSmileyVY = 0;
-    } else if (invertedSmileyY > SCREEN_HEIGHT - SPRITE_HEIGHT) {
-        invertedSmileyY = SCREEN_HEIGHT - SPRITE_HEIGHT;
-        invertedNormalizedSmileyVY = 0;
-    }
+  // Constrain p1X within screen bounds
+  if (p1X < 0) {
+    p1X = 0;
+    p1VX = 0;
+  } else if (p1X > SCREEN_WIDTH - SPRITE_WIDTH) {
+    p1X = SCREEN_WIDTH - SPRITE_WIDTH;
+    p1VX = 0;
+  }
 
-    // Calculate the distance between the two smileys
-    const distance = calculateDistance(smileyX, smileyY, invertedSmileyX, invertedSmileyY);
+  // Constrain p1Y within screen bounds
+  if (p1Y < 0) {
+    p1Y = 0;
+    p1VY = 0;
+  } else if (p1Y > SCREEN_HEIGHT - SPRITE_HEIGHT) {
+    p1Y = SCREEN_HEIGHT - SPRITE_HEIGHT;
+    p1VY = 0;
+  }
 
-    // If the distance is less than a threshold, play a beeping sound
-    const THRESHOLD_DISTANCE: f64 = 8.0; 
+  // Constrain p2X within screen bounds
+  if (p2X < 0) {
+    p2X = 0;
+    p2VX = 0;
+  } else if (p2X > SCREEN_WIDTH - SPRITE_WIDTH) {
+    p2X = SCREEN_WIDTH - SPRITE_WIDTH;
+    p2VX = 0;
+  }
+
+  // Constrain p2Y within screen bounds
+  if (p2Y < 0) {
+    p2Y = 0;
+    p2VY = 0;
+  } else if (p2Y > SCREEN_HEIGHT - SPRITE_HEIGHT) {
+    p2Y = SCREEN_HEIGHT - SPRITE_HEIGHT;
+    p2VY = 0;
+  }
+
+  // Calculate the distance between the two p1s
+  const distance = calculateDistance(p1X, p1Y, p2X, p2Y);
+
+  const p1VMagnitude = Math.sqrt(Math.pow(p1VX, 2) + Math.pow(p1VY, 2));
+  const p2VMagnitude = Math.sqrt(Math.pow(p2VX, 2) + Math.pow(p2VY, 2));
+
+  // If the distance is less than a threshold, play a beeping sound
+  const THRESHOLD_DISTANCE: f64 = 8.0;
+  if (!freezeMovement) {
     if (distance < THRESHOLD_DISTANCE) {
-        w4.tone(440, 10, 20, w4.TONE_PULSE1);
+      const cachedP1V = [p1VX, p1VY];
+      const cachedP2V = [p2VX, p2VY];
+      w4.tone(440, 10, 20, w4.TONE_PULSE1);
+      p1VX = 0;
+      p1VY = 0;
+      p2VX = 0;
+      p2VY = 0;
+      freezeMovement = true;
+      unfreezeFrame = currentFrame + freezeDuration;
+      const punchMagnitude: i32 = 3;
+      const p1Punch = punchMagnitude + (MAX_PLAYER_HEALTH - p1Health) as f32;
+      const p2Punch = punchMagnitude + (MAX_PLAYER_HEALTH - p2Health) as f32;
+
+      if (p1VMagnitude > p2VMagnitude) {
+        // player 1 does damage
+        p2VX = (cachedP1V[0] * p2Punch) as f32;
+        p2VY = (cachedP1V[1] * p2Punch) as f32;
+        p2Health--;
+      } else if (p1VMagnitude < p2VMagnitude) {
+        // player 2 does damage
+        p1VX = (cachedP2V[0] * p1Punch) as f32;
+        p1VY = (cachedP2V[1] * p1Punch) as f32;
+        p1Health--;
+      } else {
+        // equal collision
+        p1VX = -cachedP1V[0] * p1Punch;
+        p1VY = -cachedP1V[1] * p1Punch;
+        p2VX = -cachedP2V[0] * p2Punch;
+        p2VY = -cachedP2V[1] * p2Punch;
+        p1Health--;
+        p2Health--;
+      }
     }
+  }
 
-    // Draw the smiley sprite at its current position
-    w4.blit(
-        smiley,
-        smileyX,
-        smileyY,
-        SPRITE_WIDTH,        // Width of the sprite in pixels
-        SPRITE_HEIGHT,       // Height of the sprite in pixels
-        w4.BLIT_1BPP
-    );
+  // Draw the p1 sprite at its current position
+  w4.blit(
+    p1,
+    p1X,
+    p1Y,
+    SPRITE_WIDTH, // Width of the sprite in pixels
+    SPRITE_HEIGHT, // Height of the sprite in pixels
+    w4.BLIT_1BPP
+  );
 
-    // Draw the inverted smiley sprite at its current position
-    w4.blit(
-        invertedSmiley,
-        invertedSmileyX,
-        invertedSmileyY,
-        SPRITE_WIDTH,        // Width of the sprite in pixels
-        SPRITE_HEIGHT,       // Height of the sprite in pixels
-        w4.BLIT_1BPP
-    );
+  // Draw the inverted p1 sprite at its current position
+  w4.blit(
+    p2,
+    p2X,
+    p2Y,
+    SPRITE_WIDTH, // Width of the sprite in pixels
+    SPRITE_HEIGHT, // Height of the sprite in pixels
+    w4.BLIT_1BPP
+  );
 
-    // Display instructional text
-    w4.text("REAAADY... FIGHT", 18, 90);
-    w4.text("+----------------+", 10, 100);
-    
+  // Display instructional text
+  w4.text(
+    "p1:" + p2Health.toString() + " ---- " + "p2:" + p1Health.toString(),
+    10,
+    130
+  );
+  w4.text("p1 x:" + p1X.toString(), 10, 140); 
+  w4.text(" y:" + p1Y.toString(), 90, 140);
+  // do the same for p2
+  w4.text("p2 x:" + p2X.toString(), 10, 150); 
+  w4.text(" y:" + p2Y.toString(), 90, 150);
+
+  currentFrame++;
 }
-   
+
 w4.tone(120, 10, 60, w4.TONE_PULSE2);
 w4.tone(420, 15, 60, w4.TONE_PULSE2);
 w4.tone(50, 20, 60, w4.TONE_PULSE2);
